35b137446b715640a0427b2e150a271a5ba9ddb2
Geovanna Rebecca <geovannatapias0509@gmail.com>
2024-10-22 19:39:15 +0100

    Seller details and eager loading

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using SalesWeb.Data;
 using SalesWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWeb.Services
 {
@@ -11,18 +12,18 @@ namespace SalesWeb.Services
         {
             _context = context;
         }
-        public List<Seller>FindaAll() //retornar uma lista com vendedores que estão no banco de dados. 
+        public List<Seller> FindaAll() //retornar uma lista com vendedores que estão no banco de dados. 
         {
             return _context.Seller.ToList();
-        }   
+        }
         public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
         }
-        public Seller FindById (int id)
+        public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj =>obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
