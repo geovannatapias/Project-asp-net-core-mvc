5d4afb43d3efd90441b2779194079aeb44edc20a
Geovanna Rebecca <geovannatapias0509@gmail.com>
2024-11-04 18:49:31 +0000

    Implementing grouping search

--- SalesWeb/Services/SalesRecordService.cs
+++ SalesWeb/Services/SalesRecordService.cs
@@ -12,20 +12,48 @@ namespace SalesWeb.Services
         {
             _context = context;
         }
-        public async Task <List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
         {
             var result = from obj in _context.SalesRecord select obj;
-            if(minDate.HasValue) 
+            if (minDate.HasValue)
             {
-                result = result.Where(x => x.Date >= minDate.Value); 
+                result = result.Where(x => x.Date >= minDate.Value);
             }
-            if(maxDate.HasValue)
+            if (maxDate.HasValue)
             {
                 result = result.Where(x => x.Date <= maxDate.Value);
             }
             return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+
+            IQueryable<SalesRecord> query = _context.SalesRecord
+                .Include(sr => sr.Seller)
+                .ThenInclude(s => s.Department);
+
+   
+            if (minDate.HasValue)
+            {
+                query = query.Where(sr => sr.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                query = query.Where(sr => sr.Date <= maxDate.Value);
+            }
+
+            var salesRecords = await query
+                .OrderByDescending(sr => sr.Date)
+                .ToListAsync();
+
+            return salesRecords
+                .GroupBy(sr => sr.Seller.Department)
+                .ToList();
+        }
     }
 }
-    
+
+
 
--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -30,15 +30,24 @@ namespace SalesWeb.Services
         }
         public async Task RemoveAsync(int id)
         {
+            var obj = await _context.Seller.FindAsync(id);
+            if (obj == null)
+            {
+                throw new IntegrityException("Seller not found.");
+            }
+
             try
             {
-                var obj = await _context.Seller.FindAsync(id);
                 _context.Seller.Remove(obj);
                 await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException e)
             {
-                throw new IntegrityException(e.Message);
+                throw new IntegrityException("Concurrency error: " + e.Message);
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Integrity error: " + e.Message);
             }
         }
         public async Task UpdateAsync(Seller obj)
